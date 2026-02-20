55be8473d2abd0d977be4abd3cd97e62d13b5a0d
Mateus Barros <MateusBarros@ibm.com>
2026-02-20 19:55:05 -0300

    creating an interface

--- main.go
+++ main.go
@@ -6,36 +6,71 @@ import (
 	"log"
 )
 
-type Truck struct {
-	id string
+var (
+	ErrNotImplemented = errors.New("not implemented")
+	ErrTruckNotFound  = errors.New("truck not found")
+)
+
+type Truck interface {
+	LoadCargo() error
+	UnloadCargo() error
+}
+type RegularTruck struct {
+	id    string
+	cargo int
 }
 
-func processTruck(truck Truck) error {
-	fmt.Printf("Truck ID: %s\n", truck.id)
+func (t *RegularTruck) LoadCargo() error {
+	return nil
+}
+func (t *RegularTruck) UnloadCarg() error {
+	return nil
+}
 
-	return errors.New("my bad bruh")
+type EletricTruck struct {
+	id      string
+	cargo   int
+	battery float64
+}
+
+func processTruck(truck RegularTruck) error {
+
+	if err := truck.LoadCargo(); err != nil {
+		return fmt.Errorf("Error loading cargo: %s", err)
+	}
+	return ErrNotImplemented
 }
 
 func main() {
-	trucks := []Truck{
-		{id: "Truck - 1"},
-		{id: "Truck - 2"},
-		{id: "Truck - 3"},
+	trucks := []RegularTruck{
+		{id: "Truck-1"},
+		{id: "Truck-2"},
+		{id: "Truck-3"},
 	}
+	/*
+		eTrucks := []EletricTruck{
+			{id: "Eletric-truck-1"},
+		}*/
 
 	for i, truck := range trucks {
-		fmt.Printf("Truck %s has arrived:\n", truck.id)
+		fmt.Printf("Truck %s has arrived.\n", truck.id)
 
 		/*
-			err := processTruck(truck)
-			if err != nil {
+			if err := processTruck(trucks[i]); err != nil {
+				if errors.Is(err, ErrNotImplemented) {
+					continue
+				}
+
 				log.Fatalf("Error processing truck %d: %s\n", i, err)
 			}
-		*/
 
-		if err := processTruck(trucks[i]); err != nil {
+		*/
+		err := processTruck(truck)
+		if err != nil {
 			log.Fatalf("Error processing truck %d: %s\n", i, err)
 		}
+
+		fmt.Printf("Truck %s has departured.\n", truck.id)
 
 	}
 }
